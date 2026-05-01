64e1388a01adeadb60b2770f02f00d453e8ae46e
jawadamin99 <info@jawadamin.com>
2026-05-01 18:44:42 +0500

    settings.py

--- study_hub4/settings.py
+++ study_hub4/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 LOGIN_URL = '/user_login/'
 LOGIN_REDIRECT_URL = '/api/students_list/'
